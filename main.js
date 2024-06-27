1646c78c6d555aaa484084212228cc29e04e0ab2
Zaigham Haider <152426788+zaighamhaider7@users.noreply.github.com>
2024-06-27 11:56:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function darkmode(){
+    document.getElementById("body").style.backgroundColor="black"
+       document.getElementById("modes").style.backgroundColor="black"
+       document.getElementById("modes").style.transition="0.5s"
+}
+function lightmode(){
+    document.getElementById("body").style.backgroundColor="white"
+     document.getElementById("modes").style.backgroundColor="white"
+     document.getElementById("modes").style.transition="0.5s"
+}
